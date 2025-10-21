5ba3fc23239ada7aeb7a238b2e6943ca6ceaa159
Rhett Roseman <rroseman@rcplanners.com>
2025-10-21 13:30:43 -0400

    tasks.js updated to have task on page no storage but local; task is on page; task progress changes when clicked fucntions working ;

--- tasks/app/components/Navbar.js
+++ tasks/app/components/Navbar.js
@@ -10,12 +10,9 @@ export default function Navbar(){
                     Task Manager
                 </h1>
                 <div className='m-5 flex space-x-6 text-2xl'>
-                    <Link href='/' className="hover:text-green-600">Home</Link>
-                    <Link href='/Tasks'className="hover:text-green-600">Tasks</Link>
-                    <Link href='/contact'className="hover:text-green-600">Contact</Link>
-                    
-                    
-
+                 <Link href='/' className="hover:text-green-600">Home</Link>
+                 <Link href='/Tasks'className="hover:text-green-600">Tasks</Link>
+                 <Link href='/contact'className="hover:text-green-600">Contact</Link>
                 </div>
 
             </div>
---
+++ tasks/app/components/tasks.js
@@ -0,0 +1,75 @@
+'use client'
+import react from "react";
+import { useState, useEffect } from "react";
+
+export default function Tasks() {
+    const [tasks, setTasks] = useState([]) //state to hold all tasks
+    const [newTask, setNewTask] = useState("") //state to hold input field value 
+
+    useEffect(() => {
+        const saved = JSON.parse(localStorage.getItem("tasks")) || []; // Get tasks from localStorage
+        setTasks(saved);  // Set tasks state with saved tasks
+    }, []); // empty array = run only once
+
+    useEffect(() => {
+        localStorage.setItem("tasks", JSON.stringify(tasks))   // Save tasks to localStorage whenever tasks state changes
+
+    }, [tasks]) // Runs every time tasks changes
+
+    // function to add new task
+    const addTask = () => {
+        if (!newTask) return; // if no new task dont return anything
+        setTasks([...tasks, { title: newTask, status: "Not Started" }]); //add new task to tasks array
+        setNewTask('') // Clear input field
+    };
+    // function to delete task
+    const deleteTask = (index) => {
+        const updated = [...tasks]
+        updated.splice(index, 1)
+        setTasks(updated)
+    };
+    // function to change status
+    const changeStatus = (index) => {
+        const updatedTasks = [...tasks];
+        const currentStatus = updatedTasks[index].status;
+        if (currentStatus == 'Not Started') updatedTasks[index].status = 'In Progress'; 
+        else if (currentStatus === 'In Progress') updatedTasks[index].status = 'Done';
+        else updatedTasks[index].status = 'Not Started';
+
+        setTasks(updatedTasks)    
+    };
+    return (
+        <div>
+            <h2 className='text-2xl font-bold mb-4'>Tasks</h2>
+            <form className='flex mb-4'>
+                {/* Input + button wrapper */}
+                <input type='text'
+                    value={newTask}
+                    onChange={(e) => setNewTask(e.target.value)}
+                    placeholder="Add New Task"
+                    className='border-white p-2 gex-grow mr-2 rounded' />
+                <button onClick={()=> addTask()} className="bg-green-900 text-white">
+                    Add Task
+                </button>
+            </form>
+            {/* List of Task */}
+            <ul >
+                {tasks.map((task, index)=> (
+                <li key={index} className="flex justify-between items-center border-b py-2">
+                    <span>
+                        {task.title} 
+                    </span>
+                    <button onClick={() => changeStatus(index)}>
+                      :  {task.status}
+                    </button>                   
+                     <button onClick={()=> deleteTask(index)} className="bg-red-600">
+                        Delete
+                    </button>
+                </li>
+            ))}
+                
+            </ul>
+
+        </div>
+    )
+} 
