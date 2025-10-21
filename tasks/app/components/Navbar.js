6c4ed80afec050769b7e1fa7fca1efb7c55badbe
Rhett Roseman <rroseman@rcplanners.com>
2025-10-21 14:12:31 -0400

    wotdisconnected=true added to layout.js body to fix error that was arriving every time new task was started

--- tasks/app/components/Navbar.js
+++ tasks/app/components/Navbar.js
@@ -11,7 +11,7 @@ export default function Navbar(){
                 </h1>
                 <div className='m-5 flex space-x-6 text-2xl'>
                  <Link href='/' className="hover:text-green-600">Home</Link>
-                 <Link href='/Tasks'className="hover:text-green-600">Tasks</Link>
+                 {/* <Link href='/Tasks'className="hover:text-green-600">Tasks</Link> */}
                  <Link href='/contact'className="hover:text-green-600">Contact</Link>
                 </div>
 
--- tasks/app/contact/page.js
+++ tasks/app/contact/page.js
@@ -1,8 +1,10 @@
 import Contact from "../components/contact";
+import Navbar from '../components/Navbar';
 
 export default function contactPage(){
     return(
         <>
+        <Navbar/>
         <Contact/>
         </>
     )
